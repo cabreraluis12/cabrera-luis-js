7226fd299a90430e9f98e466d71f599ad9752e80
Luis Alberto Cabrera <cabreraluis1201@gmail.com>
2022-12-16 19:48:27 -0300

    defino mi array de productos del catalago

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const productos = [
+    {id:1, nombre:"Procesador Intel", precio:9900, stock:10},
+    {id:2, nombre:"Mother Intel", precio:10000, stock:5},
+    {id:3, nombre:"Memoria RAM", precio:16000, stock:100},
+    {id:4, nombre:"Disco SSD 128GB", precio:4300, stock:100},
+    {id:5, nombre:"Disco HDD 4TB", precio:27000, stock:5},
+    {id:6, nombre:"Gabinete + fuente", precio:20000, stock:1},
+];
